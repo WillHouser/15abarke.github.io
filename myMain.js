8dcd1c41b220f49be4f84ec003ecd59510b926b3
15abarke <36679352+15abarke@users.noreply.github.com>
2018-03-05 18:09:29 -0500

    Add files via upload

---
+++ myMain.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var myFunctionHolder ={};
+
+    //declaring function 1
+    myFunctionHolder.addPopups = function (feature, layer) {
+      if (feature.properties && feature.properties.Location) {
+        layer.bindPopup("<b>Address:</b>" + feature.properties.Location);
+      }
+    }
+
+    //declaring function 2
+    myFunctionHolder.pointToCircle = function (feature, latlng) {
+      var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "yellow",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      };
+      var circleMarker = L.circleMarker(latlng, geojsonMarkerOptions);
+      return circleMarker;
+    }
+
+    //execute only when window is fully loaded
+    window.onload = function () {
+
+    var mapObject = L.map('mapDivId').setView([39.99, -75.16], 11);
+    var baseMap = L.tileLayer('https://api.mapbox.com/styles/v1/12382/cjci5jqqc7xag2rlp6inpmdea/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiMTIzODIiLCJhIjoiY2pjaTRscWllMmV0ZTMzbnptZXppczA1MCJ9.DbUREsZcMMNHEBwqytnWnA', {
+      maxZoom: 18,
+      attribution: "&copy; <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> &copy; <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a>"
+    }).addTo(mapObject);
+    var cfg = {
+      "radius": 0.005,
+      "maxOpacity": .8,
+      "scaleRadius": true,
+      "uselocalExtrema": true,
+      latField: 'lat',
+      lngField: 'lng',
+      valueField: 'value'
+    };
+    var heatmapLayer= new HeatmapOverlay(cfg);
+    heatmapLayer.setData(theftsHeatMapData);
+    mapObject.addLayer(heatmapLayer);
+    // bikeThefts is the variable name we difined in Bike_Thefts_2011.js file. 
+    //var bikesLayerGroup = L.geoJSON(bikeThefts, {
+    //  onEachFeature: myFunctionHolder.addPopups,
+    //  pointToLayer: myFunctionHolder.pointToCircle
+    //});
+
+    //mapObject.addLayer(bikesLayerGroup);
+    //mapObject.fitBounds(bikesLayerGroup.getBounds());
+};
